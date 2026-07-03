38cf415fd57f1903c49048a54db0c1dfc38713f7
sita12 <sitaneupane5700@gmail.com>
2026-07-03 20:36:04 +0545

    added user request test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
   before(:all) do
     @user1 = FactoryBot.create(:user)
   end
-  
+
   it "is valid with valid attributes" do
     expect(@user1).to be_valid
   end
-  
+
   it "has a unique email" do
     user1 = FactoryBot.build(:user)
     expect(user1).to_not be_valid
   end
 
-  it "is not valid without a email" do 
+  it "is not valid without a email" do
     user1 = FactoryBot.build(:user, email: nil)
     expect(user1).to_not be_valid
   end
 
-  it "is not valid without a first_name" do 
+  it "is not valid without a first_name" do
     user1 = FactoryBot.build(:user, email: "johnvoy1@gmail.com", first_name: nil)
     expect(user1).to_not be_valid
   end
 
-  it "is not valid without a last_name" do 
+  it "is not valid without a last_name" do
     user1 = FactoryBot.build(:user, email: "johnvoy1@gmail.com", last_name: nil)
     expect(user1).to_not be_valid
   end
 
-  it "is not valid without a password" do 
+  it "is not valid without a password" do
     user1 = FactoryBot.build(:user, email: "johnvoy1@gmail.com", password: nil)
     expect(user1).to_not be_valid
   end
---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Users", type: :request do
+  describe ' controller test cases ' do
+    let(:user1) { FactoryBot.create :user }
+
+    describe '#create' do
+      it 'should give status 200' do
+        post "/api/v1/users/signup", params: { first_name: "John", last_name: "Doe", email: "john1@email.com", password: "very_secret", country: "US" }, as: :json
+        expect(response.status).to eq(200)
+        expect(response).to match_json_schema("user")
+      end
+      it 'should give status 422' do
+        post "/api/v1/users/signup", params: {}, as: :json
+        expect(response.status).to eq(422)
+      end
+    end
+
+    describe '#login' do
+      it 'should give status 200' do
+        post "/api/v1/auth/signin", params: { auth: { email: "johnvon@gmail.com", password: "verysecret" } }, as: :json
+        expect(response.status).to eq(200)
+        expect(response).to match_json_schema("user")
+      end
+      it 'should give status 400' do
+        post "/api/v1/auth/signin", params: { auth: { email: "johnvon@gmail.com", password: "secret" } }, as: :json
+        expect(response.status).to eq(400)
+      end
+    end
+  end
+end
