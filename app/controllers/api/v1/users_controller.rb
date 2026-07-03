9814d0baf53d62d1fda025443d5300e3e23ab33c
sita12 <sitaneupane5700@gmail.com>
2026-07-03 20:35:32 +0545

    added user apis

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::UsersController < ApiController
+  before_action :set_user, only: [ :login ]
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @token = JsonWebToken.encode(user_id: @user.id)
+      render "show"
+    else
+      render json: { message: @user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    if @user && @user.authenticate(auth_params[:password])
+      @token = JsonWebToken.encode(user_id: @user.id)
+      render "show"
+    else
+      render json: { message: "Error: Invalid email or password" }, status: 400
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find_by_email(auth_params[:email])
+  end
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password, :country)
+  end
+
+  def auth_params
+    params.require(:auth).permit(:email, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  ##VALIDATIONS
+  # #VALIDATIONS
   validates_presence_of :first_name, :last_name, :email, :password
   validates_uniqueness_of :email
+
+  ## Methods
+  def name
+    first_name + " " + last_name
+  end
 end
