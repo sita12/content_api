bb0b7dab2dae2e1e442d7c3a30b65cdb8310a02d
sita12 <sitaneupane5700@gmail.com>
2026-07-03 16:08:34 +0545

    added user test cases

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password_digest { "MyString" }
+    first_name { "John" }
+    last_name { "Von" }
+    email { "johnvon@gmail.com" }
+    password { "verysecret" }
+    country { "US" }
   end
 end
